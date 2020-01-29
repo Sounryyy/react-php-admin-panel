cf1207d7d89d0023473c047ef5618ae1fa6d9114
Sounryyy <justtrueserj@gmail.com>
2020-01-29 20:19:00 +0300

    Extended gulpfile, package json && Refactored index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,46 @@
 const gulp = require("gulp");
+const webpackStream = require("webpack-stream");
+
+const dist = '/Applications/MAMP/htdocs/react-admin/admin';
+const webpackConfig = {
+    mode: 'development',
+    output: {
+        filename: 'script.js'
+    },
+    watch: false,
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                debug: true,
+                                corejs: 3,
+                                useBuiltIns: "usage"
+                            }],
+                            "@babel/react"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+
+};
 
 gulp.task("copy-html", () => {
     return gulp.src("./app/src/index.html")
-        .pipe(gulp.dest("/Applications/MAMP/htdocs/react-admin/admin"))
+        .pipe(gulp.dest(dist))
+});
+
+
+gulp.task("build-js", () => {
+   return gulp.src("./app/src/main.js")
+       .pipe(webpackStream(webpackConfig))
+       .pipe(gulp.dest(dist))
 });
